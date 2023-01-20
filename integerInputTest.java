c048035f069e4d2a87163b57576f2f3c9c7e8ad4
AdeenaSyeda <97324527+AdeenaSyeda@users.noreply.github.com>
2023-01-20 10:01:30 +0500

    Add files via upload

---
+++ integerInputTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class integerInputTest {
+
+	@Test
+	public void test() {
+		integerInput i = new integerInput();
+		System.out.print(i.isEven(7)+"\n");
+		System.out.print(i.isPrime(3));
+	}
+
+}
